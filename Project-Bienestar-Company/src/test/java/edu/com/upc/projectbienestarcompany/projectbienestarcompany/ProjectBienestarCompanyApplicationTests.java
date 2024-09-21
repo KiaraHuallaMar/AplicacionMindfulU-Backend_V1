ca9efc0ea03c12f91369b6c218dd10b14a526dc2
Aldair Pariño <u202020356@upc.edu.pe>
2024-09-21 00:49:49 -0500

    creacion del proyecto
    creacion del proyecto

---
+++ Project-Bienestar-Company/src/test/java/edu/com/upc/projectbienestarcompany/projectbienestarcompany/ProjectBienestarCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.com.upc.projectbienestarcompany.projectbienestarcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBienestarCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
