2991e0cfb2e055142d542f73a83eb69e7a612bc4
aldairpc15 <u202020356@upc.edu.pe>
2024-09-25 16:13:25 -0500

    Implementacion de Repository
    Implementacion de Repository de Grupo Apoyo  y Cita

---
+++ repositories/GrupoApoyoRepository.java
@@ -0,0 +1,7 @@
+package edu.com.upc.mindfulu.mindfulu.repositories;
+
+import edu.com.upc.mindfulu.mindfulu.entities.GrupoApoyo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GrupoApoyoRepository extends JpaRepository<GrupoApoyo, Integer> {
+}
