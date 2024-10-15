2c90ff05b59789643674a356209b23468ec154a3
aldairpc15 <u202020356@upc.edu.pe>
2024-10-15 10:57:08 -0500

    Query Cita

--- repositories/CitaRepository.java
+++ repositories/CitaRepository.java
@@ -4,4 +4,10 @@ import edu.com.upc.mindfulu.mindfulu.entities.Cita;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CitaRepository extends JpaRepository<Cita, Integer> {
+
+  //TotalRecaudado de Cita
+   @Query(value = "select sum(cita.precio_cita) as total_recaudado\n" +
+            "from cita inner join cita \n" +
+            "on usuario.id = cita.usuario_id",nativeQuery = true)
+    public List<String[]> totalRecaudado();
 }
