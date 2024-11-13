7d27a7bd96332a78180fcff84788bfa9f245ba12
aldairpc15 <u202020356@upc.edu.pe>
2024-11-13 18:37:17 -0500

    Delete repositories directory

--- repositories/GrupoApoyoRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.com.upc.mindfulu.mindfulu.repositories;
-
-import edu.com.upc.mindfulu.mindfulu.entities.GrupoApoyo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GrupoApoyoRepository extends JpaRepository<GrupoApoyo, Integer> {
-}
